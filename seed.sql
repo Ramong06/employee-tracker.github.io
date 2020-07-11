ecd10e90b612ec5754e2ca07411a99bacd5d36d6
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-10 19:32:55 -0500

    added all roles into seed.sql

--- seed.sql
+++ seed.sql
@@ -14,6 +14,12 @@ INSERT INTO role
 VALUES
 ("Department Supervisor", 90000, 1),
 ("Department Manager", 75000, 1),
+("HR Director", 100000, 2),
+("HR Manager", 75000, 2),
+("Accountant", 55000, 3),
+("IT Analyst", 80000, 4),
+("IT Director", 120000, 4),
 ("Sales Director", 180000, 5),
 ("Outside Sales", 120000, 5),
 ("Inside Sales", 100000, 5),
+("Customer Service Representative", 34000, 6),
