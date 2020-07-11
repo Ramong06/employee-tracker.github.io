0850e52f033e0527b0f2772de50f28d9ce8618c1
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-10 19:20:10 -0500

    still adding more roles, salary and id into seed.sql

--- seed.sql
+++ seed.sql
@@ -2,12 +2,18 @@ USE employee_trackerDB;
 
 INSERT INTO department
 VALUES
-("Administration"),
+("Management"),
 ("Human Resources"),
 ("Accounting"),
 ("IT"),
-("Sales");
+("Sales"),
+("Customer Service");
 
 INSERT INTO role
+(title, salary, department_id)
 VALUES
-(
+("Department Supervisor", 90000, 1),
+("Department Manager", 75000, 1),
+("Sales Director", 180000, 5),
+("Outside Sales", 120000, 5),
+("Inside Sales", 100000, 5),
