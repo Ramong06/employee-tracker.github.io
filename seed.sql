49669853ec53a947d13fbeeb5a8f88033915e1e1
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-10 19:40:54 -0500

    beginning to add employee details to seed.sql

--- seed.sql
+++ seed.sql
@@ -23,3 +23,8 @@ VALUES
 ("Outside Sales", 120000, 5),
 ("Inside Sales", 100000, 5),
 ("Customer Service Representative", 34000, 6),
+
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+VALUES
+()
