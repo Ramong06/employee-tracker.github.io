b80e8a36bad0d040ab69167f3a394791779b0b59
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-13 23:48:42 -0500

    changed single quotes to double quotes in seed

--- seed.sql
+++ seed.sql
@@ -2,30 +2,30 @@ USE employee_trackerDB;
 
 INSERT INTO department
 VALUES
-(1, 'Management'),
-(2, 'Human Resources'),
-(3, 'Accounting'),
-(4, 'IT'),
-(5, 'Sales'),
-(6, 'Customer Service');
+(1, "Management"),
+(2, "Human Resources"),
+(3, "Accounting"),
+(4, "IT"),
+(5, "Sales"),
+(6, "Customer Service");
 
 INSERT INTO role
 (title, salary, department_id)
 VALUES
-('Department Supervisor', 90000, 1),
-('Department Manager', 75000, 1),
-('HR Director', 100000, 2),
-('HR Manager', 75000, 2),
-('Accountant', 55000, 3),
-('IT Analyst', 80000, 4),
-('IT Director', 120000, 4),
-('Sales Director', 180000, 5),
-('Outside Sales', 120000, 5),
-('Inside Sales', 100000, 5),
-('Customer Service Representative', 34000, 6),
+("Department Supervisor", 90000, 1),
+("Department Manager", 75000, 1),
+("HR Director", 100000, 2),
+("HR Manager", 75000, 2),
+("Accountant", 55000, 3),
+("IT Analyst", 80000, 4),
+("IT Director", 120000, 4),
+("Sales Director", 180000, 5),
+("Outside Sales", 120000, 5),
+("Inside Sales", 100000, 5),
+("Customer Service Representative", 34000, 6),
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-('Sheev', 'Palpatine', 1, ),
-('Darth', 'Vader', 1, ),
+("Sheev", "Palpatine", 1, ),
+("Darth", "Vader", 1, ),
